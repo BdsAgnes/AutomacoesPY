414c38a1b610d9c2bfcdf0a0a999fa7aa597d70c
bdsagnes <bdsagnes@gmail.com>
2023-08-19 16:09:37 -0300

    projeto de automação para oganizar arquivos

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title="Selecione uma pasta")
+# lista todos arquivos que estão dentro desse caminho
+lista_arquivos = os.listdir(caminho)
+''' dicionario de arquivos com o nome pasta e
+as estensões de arquivos que irá na quela pasta '''
+locais = {
+    "imagens": [".png", ".jpg"],
+    "planilhas": [".xlsx"],
+    "pdfs": [".pdf"],
+    "csv": [".csv"],
+}
+
+for arquivo in lista_arquivos:
+    
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            # verifica se a pasta existe
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                # caso não, vai criar
+                os.mkdir(f"{caminho}/{pasta}")
+                # reorganizando os arquivos
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
